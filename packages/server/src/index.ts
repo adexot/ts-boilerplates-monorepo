647970b560c3d0d60ad204a1de950605c9809541
Adekunle Oseni <adekunleoseni47@gmail.com>
2023-01-01 18:14:30 +0100

    chore(server): add a function to catch server shutdown

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,16 +5,29 @@ import logger from './utilities/logger.util';
 const PORT = 8081;
 const app = express();
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+async function startServer() {
+  app.use(express.json()); // for parsing application/json
+  app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => {
-  logger.info('hitting the root path => /');
-  res.send('Welcome to the react-node-monorepo');
-});
+  // default path for the API
+  app.get('/', (req, res) => {
+    // refactor this out to a middleware for simplicity
+    logger.info('hitting the root path => /');
+    res.send('Welcome to the react-node-monorepo');
+  });
 
-registerRoutes(app);
+  // api routes register
+  registerRoutes(app);
 
-app.listen(PORT, () => {
-  logger.info(`Server is running at http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    logger.info(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+const logErrorAndExit = (err: ErrorEvent) => {
+  logger.error(err);
+  process.exit();
+};
+
+// init server
+startServer().catch(logErrorAndExit);
